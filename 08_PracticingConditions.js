3f8587d5716ac31af5e8e3581cc2571c15b468f2
RonitBanga <bangaronit22@gmail.com>
2024-01-20 23:00:50 +0530

    Jan 20, 2024

---
+++ 08_PracticingConditions.js
@@ -0,0 +1,37 @@
+/*
+let age = prompt("What is your age?");
+if(age>10 && age<20) {
+  console.log("Your age lies between 10 ans 20");
+}else{
+  console.log("Your age doesn't lies between 10 ans 20");
+}
+*/
+/*
+let age = 13;
+switch (age) {
+  case 12:
+    console.log("Your age is 12");
+    break;
+  case 13:
+    console.log("Your age is 13");
+    break;
+  case 14:
+    console.log("Your age is 14");
+  case 15:
+    console.log("Your age is 15");
+    break;
+  default:
+    console.log("Your age is not special");
+}
+*/
+/*
+let age = 19;
+if (age%2==0 && age%3==0) {
+  console.log ("Number is divisible by 2 and 3");
+} else {
+  console.log ("Number is not divisible by 2 and 3");
+}
+*/
+let age = 19
+let a = age <= 18 ? "You can't DRIFT" : "You can DRIFT";
+console.log(a);
