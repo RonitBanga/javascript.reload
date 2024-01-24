9840bdde3327ea8ff9ddce9733a5f370f410eeec
RonitBanga <bangaronit22@gmail.com>
2024-01-25 00:00:57 +0530

    Jan 24 2024

---
+++ 14_StringMethods.js
@@ -0,0 +1,21 @@
+let n = "Ronit\'";
+console.log(n.length);
+
+let name = "Ronit"
+
+console.log(name);
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+console.log(name.slice(2,4));
+console.log(name.slice(2));
+
+name = "Ronit bhai";
+console.log(name.replace("bhai","Boss"));
+
+let friend = "Naman";
+
+console.log(name.concat(" is a friend of ", friend, " ok"));
+
+friend = "        Ronit ";
+console.log(friend);
+console.log(friend.trim());
