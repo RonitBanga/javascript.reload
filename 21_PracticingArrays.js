084dafa2358e518d9f262638e86a50620155f078
RonitBanga <bangaronit22@gmail.com>
2024-02-06 19:47:42 +0530

    Feb 06, 2023

---
+++ 21_PracticingArrays.js
@@ -0,0 +1,42 @@
+
+// Add a number at the ending of the array
+// let arr = [1, 2, 3, 4, 5];
+// let a = prompt("Enter a number : ");
+// a = Number.parseInt(a);
+// arr.push(a);
+// console.log(arr);
+
+
+// Add more than 1 number
+// let arr = [];
+// let a;
+// do{
+//     a = prompt("Enter a number : ");
+//     a = Number.parseInt(a);
+//     arr.push(a);
+// }while(a!=0)
+// console.log(arr);
+
+
+// filter number divisible by 10
+// let arr = [1, 2, 30, 4, 70, 80];
+// let a = arr.filter((x)=>{
+//     return x%10 == 0;
+// });
+// console.log(a);
+
+
+// create a array of sq of number
+// let arr = [1, 2, 30, 4, 70, 80];
+// let a = arr.map((x)=>{
+//     return x*x;
+// });
+// console.log(a);
+
+
+// calculate factorial using reduce
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let a = arr.filter((x1, x2)=>{
+    return x1*x2;
+});
+console.log(a);
